a682f6898c19945356b0b6158f8765a1bd9595a9
Alexandre da Silva <lexrupy@gmail.com>
2023-07-31 00:47:09 -0300

    config null-ls

--- plugins.lua
+++ plugins.lua
@@ -1,13 +1,25 @@
 local plugins = {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "pyright",
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "pyright",
+        -- "mypy",
+        -- "ruff",
+      },
     },
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
+    config = function() 
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
